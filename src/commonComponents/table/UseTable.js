15bcbfd588f749ac86c2ff96c1011cb1822d0b6c
Muhammad Sami Shoukat <88966319+samishoukat12@users.noreply.github.com>
2022-02-14 16:45:04 +0500

    searchbar modify

--- src/commonComponents/table/UseTable.js
+++ src/commonComponents/table/UseTable.js
@@ -3,6 +3,11 @@ import React, { useState } from 'react'
 export function UseTable() {
   const [searchQuery, setSearchQuery] = useState('')
   const [searchShow, setSearchShow] = useState(false)
+
+
+
+
+
   const onTextChangeHandler = (val) => {
     setSearchQuery(val)
   };
@@ -12,17 +17,9 @@ export function UseTable() {
   const searchingFor = (searchQuery) => {
     return function (data) {
       return (
-        data?.name.toLowerCase().includes(
-          searchQuery.toLowerCase(),
-        ) ||
-        data?.email.toLowerCase().includes(
+        (data?.name || data?.Title).toLowerCase().includes(
           searchQuery.toLowerCase(),
         )
-        ||
-        data?.phone.toLowerCase().includes(
-          searchQuery.toLowerCase())
-        ||
-        !searchQuery
       );
     };
   };

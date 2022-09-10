ec693363a77465d3e8e5b824d5afbb68cc7fde6e
umairm1alik <umairm1alik@gmail.com>
2022-09-10 16:11:27 +0500

    clean code

--- src/modules/adminPortal/contactUs/UseContactUs.js
+++ src/modules/adminPortal/contactUs/UseContactUs.js
@@ -189,32 +189,11 @@ export function UseContactUs() {
 
 
     //Update Contacts
-    const updateContactsInCache = (cache, { data }) => {
-        const updateContact = data
-        console.log("data of update Event", updateContact);
-        console.log("cache", cache);
-        const contacts = cache.readQuery({
-            query: GET_CONTACT_US,
-        })
-        console.log("Existing Events", contacts.contactuses);
-
-        // cache.writeQuery({
-        //     query: GET_EVENTS,
-        //     data: {
-        //         findManyEvents: [
-        //             ...events.findManyEvents,
-        //             newEvent
-        //         ]
-        //     }
-        // })
-    };
     let [
         UpdateContactUs,
         {
             loading: UPDATE_LOADING
-        }] = useMutation(UPDATE_SINGLE_CONTACT, {
-            update: updateContactsInCache
-        });
+        }] = useMutation(UPDATE_SINGLE_CONTACT);
 
     const ctaUpdateHandler = async (event) => {
         event.preventDefault();
